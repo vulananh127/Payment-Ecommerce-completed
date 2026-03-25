c5cd2f542fbdff81a6c7a22d7ee162119331e44a
vulananh127 <vulananh127@gmail.com>
2026-03-25 13:54:21 +0700

    little fix

--- src/main/java/com/Payment/Shop/dto/request/VNPaymentRequest.java
+++ src/main/java/com/Payment/Shop/dto/request/VNPaymentRequest.java
@@ -1,10 +1,12 @@
 package com.Payment.Shop.dto.request;
 
+import java.math.BigDecimal;
+
 import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class VNPaymentRequest extends PaymentRequest{
-    protected double totalAmount;
+    protected BigDecimal totalAmount;
 }
